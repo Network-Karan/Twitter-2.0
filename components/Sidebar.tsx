8e8afbe2a669959a1d9efaceb98815075b847fb9
Karan <kaliyaperumal.diwakaran@gmail.com>
2022-05-21 15:12:57 +0530

    init

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -13,9 +13,15 @@ import SidebarRow from './SidebarRow'
 
 function Sidebar() {
   return (
-    <div className="h-10 w-10">
-        <img src="https://links.papareact.com/drq" alt="" />
+    <div>
+        <img className="5-10 w-10" src="https://links.papareact.com/drq" alt="" />
         <SidebarRow Icon={HomeIcon} title="Home" />
+        <SidebarRow Icon={HashtagIcon} title="Explore" />
+        <SidebarRow Icon={BellIcon} title="Notification" />
+        <SidebarRow Icon={MailIcon} title="Messages" />
+        <SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
+        <SidebarRow Icon={CollectionIcon} title="Lists" />
+
     </div>
   )
 }
