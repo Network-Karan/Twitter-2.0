473f70a202f6b77ce697c09e7326fa8ecdc03e95
Karan <kaliyaperumal.diwakaran@gmail.com>
2022-05-21 14:57:53 +0530

    init

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import {
+    BellIcon,
+    HashtagIcon,
+    BookmarkIcon,
+    CollectionIcon,
+    DotsCircleHorizontalIcon,
+    MailIcon,
+    UserIcon,
+    HomeIcon
+} from '@heroicons/react/outline'
 
 function Sidebar() {
   return (
-    <div>Ima Sidebar</div>
+    <div>
+        <img src="https://links.papareact.com/drq" alt="" />
+    </div>
   )
 }
 
-export default Sidebar
+export default Sidebar 
