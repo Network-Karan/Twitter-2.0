eb7e1d1384db86e597c55e0016a3ad32202852d8
Karan <kaliyaperumal.diwakaran@gmail.com>
2022-05-21 19:18:29 +0530

    init

--- pages/Feed.tsx
+++ pages/Feed.tsx
@@ -5,7 +5,7 @@ function Feed() {
   return (
     <div>
         <div>
-            <h1>Home</h1>
+            <h1 className= "p-5 pb-0 text-xl font-bold">Home</h1>
             <RefreshIcon  className="mr-5 mt-5 h-8 w-8 cursor-pointer text-twitter transition-all duration-500 ease-out
             hover:rotate-180 active:scale-125" />
         </div>
