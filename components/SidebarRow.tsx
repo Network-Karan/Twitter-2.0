07e64fa61dba848185c68b1c9ec7b762fc232378
Karan <kaliyaperumal.diwakaran@gmail.com>
2022-05-21 17:40:51 +0530

    init

--- components/SidebarRow.tsx
+++ components/SidebarRow.tsx
@@ -8,9 +8,9 @@ interface Props {
 
 function SidebarRow({Icon, title}: Props) {
   return (
-    <div>
+    <div className="group flex items-center space-x-2 rounded-full px-4 py-3 transition-all duration-200 hover:bg-gray-100">
         <Icon className="h-6 w-6" />
-        <p>{title}</p>
+        <p className="group-hover:text-blue-300">{title}</p>
     </div> 
   )
 }
