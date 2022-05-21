b9c09a411d0e995867042047437bb0a88b3f984c
Karan <kaliyaperumal.diwakaran@gmail.com>
2022-05-21 22:13:35 +0530

    init

--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -4,12 +4,12 @@ import React from 'react'
 function Widgets() {
   return (
     <div>
-      <div className="flex items-center space-x-2 bg-gray-100">
+      <div className="flex items-center space-x-2 rounded-full bg-gray-100 p-3">
         <SearchIcon className="h-5 w-5" />
         <input 
         type="text" 
         placeholder="Search Twitter"
-        className="bg-transparent"
+        className="flex-1 bg-transparent"
          /> 
       </div>
     </div>
