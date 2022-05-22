aa516628c04e1a9d26d749e54074aa3439189906
Karan <kaliyaperumal.diwakaran@gmail.com>
2022-05-22 15:33:38 +0530

    init

--- components/TweetBox.tsx
+++ components/TweetBox.tsx
@@ -1,20 +1,33 @@
 import React from 'react'
+import {
+  CalendarIcon,
+  EmojiHappyIcon,
+  LocationMarkerIcon,
+  PhotographIcon,
+  SearchCircleIcon,
+} from '@heroicons/react/outline'
 
 function TwitterBox() {
   return (
-    <div>
+    <div className="flex space-x-2 p-5">
         <img 
         className=" mt-4 h-14 w-14 rounded-full object-cover"
         src="https://links.papareact.com/gll" 
         alt="" />
 
-        <div>
-          <form>
+        <div className="flex flex-1 pl-2">
+          <form className="flex flex-1 flex-col">
             <input type="text" placeholder="What's Happening ?" />
-            <div>
-              <div>{/*Icons*/}</div>
+            <div className="flex items-center">
+              <div className="flex flex-1 space-x-2 text-twitter">
+                <PhotographIcon className="h-5 w-5" />
+                <SearchCircleIcon className="h-5 w-5" />
+                <EmojiHappyIcon className="h-5 w-5" />
+                <CalendarIcon className="h-5 w-5" />
+                <LocationMarkerIcon className="h-5 w-5" />
+              </div>
 
-              <button>Tweet</button>
+              <button className="rounded-full bg-twitter px-5 py-2 font-bold text-white ">Tweet</button>
             </div>
           </form>
         </div>
