c5225586b9834e722ed742b899bc3c4ae9ba6009
Karan <kaliyaperumal.diwakaran@gmail.com>
2022-05-22 10:45:17 +0530

    init

--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -1,17 +1,25 @@
 import { SearchIcon } from '@heroicons/react/outline'
 import React from 'react'
+import {TwitterTimelineEmbed} from 'react-twitter-embed'
 
 function Widgets() {
   return (
-    <div>
+    <div className="mt-2 px-2">
+      {/*Search*/}
       <div className="flex items-center space-x-2 rounded-full bg-gray-100 p-3">
         <SearchIcon className="h-5 w-5" />
         <input 
         type="text" 
         placeholder="Search Twitter"
-        className="flex-1 bg-transparent"
+        className="flex-1 bg-transparent outline-none"
          /> 
       </div>
+
+      <TwitterTimelineEmbed
+  sourceType="profile"
+  screenName="Network_Karan"
+  options={{height: 1000}}
+/>
     </div>
   )
 }
